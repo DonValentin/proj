dab8722451fb2b187db7763ef5b42048878ae0e9
Разгуляев Валентин Максимович (4055105) <razgulyaev@vtb.ru>
2023-05-01 15:05:33 +0300

    Добавлен класс коробки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,12 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("11111");
+        System.out.println("0000");
+        System.out.println("2222");
+        System.out.println("3333");
+    }
+
+        public static void d() {
+        System.out.println(1);
     }
 }
