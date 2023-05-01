e8f9c4052f7273ba5b6c9cafdce27850099e7c50
Разгуляев Валентин Максимович (4055105) <razgulyaev@vtb.ru>
2023-05-01 15:13:02 +0300

    Выполнили таск2

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,13 +3,13 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("0000");
+        System.out.println("----");
         System.out.println("2222");
         System.out.println("3333");
     }
 
         public static void d() {
-        System.out.println(1);
+        System.out.println(0);
         System.out.println(1);
         System.out.println(1);
         System.out.println(1);
